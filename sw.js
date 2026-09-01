575c0d8e345d18b6863f49d33741c610c10b2b87
kevinmdoca-hub <kevin.mdoca@gmail.com>
2026-08-31 19:58:28 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const PREFIX='clover-inspection-es-';
 const LEGACY_PREFIX='clover-inspeccion-';
-const CACHE=PREFIX+'v078-20260831';
-const ASSETS=['./','./index.html','./app.js?v=078','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+const CACHE=PREFIX+'v079-20260831';
+const ASSETS=['./','./index.html','./app.js?v=079','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>(k.startsWith(PREFIX)||k.startsWith(LEGACY_PREFIX))&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return resp}).catch(()=>caches.match('./index.html'))))});
